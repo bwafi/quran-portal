aaf60913d01dbd6e39db2238b67fd0935626ec4d
bwafi <syahronibwf@gmail.com>
2023-06-26 16:21:46 +0700

    show modal animation

--- components/detailSurah/DetailSurahComponent.jsx
+++ components/detailSurah/DetailSurahComponent.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Ayat from "./Ayat";
 import { PiInfo } from "react-icons/pi";
 import TafsirSurah from "./TafsirSurah";
 
 const DetailSurahComponent = ({ detailSurah, id }) => {
+  const [showModal, setShowModal] = useState(false);
+
   const { preBismillah } = detailSurah;
+
+  const handleShowModal = () => {
+    setShowModal(true);
+  };
 
   return (
     <>
@@ -17,14 +23,14 @@ const DetailSurahComponent = ({ detailSurah, id }) => {
               <Image src="../logo/basmalah.svg" alt="basmalah" width={250} height={250} priority />
             </div>
           )}
-          <button className="flex items-center gap-1">
+          <button onClick={handleShowModal} className="flex items-center gap-1">
             <PiInfo className="text-xl" /> <span className="text-sm">Info Surah</span>
           </button>
         </div>
         <div className="w-full">
           <Ayat detailSurah={detailSurah} />
         </div>
-        <TafsirSurah id={id} />
+        <TafsirSurah id={id} showModal={showModal} setShowModal={setShowModal} />
       </div>
     </>
   );
--- components/detailSurah/TafsirSurah.jsx
+++ components/detailSurah/TafsirSurah.jsx
@@ -4,8 +4,9 @@ import { getDetailSurah, selectDetailSurah } from "@/store/slice/contentSlice";
 import React, { useEffect } from "react";
 import { MdClose } from "react-icons/md";
 import { useDispatch, useSelector } from "react-redux";
+import { AnimatePresence, motion } from "framer-motion";
 
-const TafsirSurah = ({ id }) => {
+const TafsirSurah = ({ id, setShowModal, showModal }) => {
   const surahDetail = useSelector(selectDetailSurah);
 
   const dispatch = useDispatch();
@@ -13,44 +14,65 @@ const TafsirSurah = ({ id }) => {
     dispatch(getDetailSurah(id));
   }, [dispatch, id]);
 
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
-    <div className="fixed w-full h-screen top-0 left-0 mx-auto z-40">
-      <div className="w-full h-screen bg-black/40 z-0 mx-auto">
-        <div className="w-7/12 overflow-y-auto mx-auto bg-white relative top-20 rounded-lg ">
-          <div className="flex justify-between items-center py-5 border-b border-gray-200 px-5 shadow-sm">
-            <h3 className="font-bold text-lg">Surah {surahDetail.name.transliteration.id}</h3>
-            <button>
-              <MdClose className="text-3xl" />
-            </button>
-          </div>
-          <div className="py-5 overflow-y-auto relative max-h-96 px-5">
-            <div className="mb-3">
-              <h4 className="font-bold flex gap-2">
-                Terjemah : <p>{surahDetail.name.translation.id}</p>
-              </h4>
-              <h4 className="font-bold flex gap-2">
-                Tempat Diturunkan : <p>{surahDetail.revelation.id}</p>
-              </h4>
-              <h4 className="font-bold flex gap-2">
-                Jumlah Ayat : <p>200</p>
-              </h4>
-            </div>
-            <p className="text-lg">
-              "Surat Al Faatihah (Pembukaan) yang diturunkan di Mekah dan terdiri dari 7 ayat adalah surat yang
-              pertama-tama diturunkan dengan lengkap diantara surat-surat yang ada dalam Al Quran dan termasuk golongan
-              surat Makkiyyah. Surat ini disebut Al Faatihah (Pembukaan), karena dengan surat inilah dibuka dan
-              dimulainya Al Quran. Dinamakan Ummul Quran (induk Al Quran) atau Ummul Kitaab (induk Al Kitaab) karena dia
-              merupakan induk dari semua isi Al Quran, dan karena itu diwajibkan membacanya pada tiap-tiap sembahyang.
-              Dinamakan pula As Sab'ul matsaany (tujuh yang berulang-ulang) karena ayatnya tujuh dan dibaca
-              berulang-ulang dalam sholat."
-            </p>
-          </div>
-          <div className="border-t border-gray-300 py-3 flex justify-end px-5">
-            <button className="py-2 px-5 bg-primary rounded-lg text-white">Close</button>
-          </div>
+    <AnimatePresence>
+      {showModal && (
+        <div className="fixed w-full h-screen top-0 left-0 mx-auto z-40">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3, ease: [0.25, 0.46, 0.45, 0.94] }}
+            key={surahDetail.number}
+            className="w-full h-screen bg-black/40 z-0 mx-auto">
+            <motion.div
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -20 }}
+              transition={{ duration: 0.3, ease: [0.25, 0.46, 0.45, 0.94] }}
+              className="w-7/12 overflow-y-auto mx-auto bg-white relative top-20 rounded-lg ">
+              <div className="flex justify-between items-center py-5 border-b border-gray-200 px-5 shadow-sm">
+                <h3 className="font-bold text-lg">Surah {surahDetail.name.transliteration.id}</h3>
+                <button onClick={handleCloseModal}>
+                  <MdClose className="text-3xl" />
+                </button>
+              </div>
+              <div className="py-5 overflow-y-auto relative max-h-96 px-8">
+                <div className="mb-3">
+                  <h4 className="font-bold flex gap-2">
+                    Terjemah : <p>{surahDetail.name.translation.id}</p>
+                  </h4>
+                  <h4 className="font-bold flex gap-2">
+                    Tempat Diturunkan : <p>{surahDetail.revelation.id}</p>
+                  </h4>
+                  <h4 className="font-bold flex gap-2">
+                    Jumlah Ayat : <p>200</p>
+                  </h4>
+                </div>
+                <p className="text-lg">
+                  "Surat Al Faatihah (Pembukaan) yang diturunkan di Mekah dan terdiri dari 7 ayat adalah surat yang
+                  pertama-tama diturunkan dengan lengkap diantara surat-surat yang ada dalam Al Quran dan termasuk
+                  golongan surat Makkiyyah. Surat ini disebut Al Faatihah (Pembukaan), karena dengan surat inilah dibuka
+                  dan dimulainya Al Quran. Dinamakan Ummul Quran (induk Al Quran) atau Ummul Kitaab (induk Al Kitaab)
+                  karena dia merupakan induk dari semua isi Al Quran, dan karena itu diwajibkan membacanya pada
+                  tiap-tiap sembahyang. Dinamakan pula As Sab'ul matsaany (tujuh yang berulang-ulang) karena ayatnya
+                  tujuh dan dibaca berulang-ulang dalam sholat."
+                </p>
+              </div>
+              <div className="border-t border-gray-300 py-3 flex justify-end px-5">
+                <button onClick={handleCloseModal} className="py-2 px-5 bg-primary rounded-lg text-white">
+                  Close
+                </button>
+              </div>
+            </motion.div>
+          </motion.div>
         </div>
-      </div>
-    </div>
+      )}
+    </AnimatePresence>
   );
 };
 
